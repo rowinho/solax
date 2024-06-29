ccefc362e0b760890421fe3773da77ffb98d788d
Roman Windischhofer <roman.windischhofer@gmail.com>
2024-06-29 20:27:14 +0200

    Reduce to X3HypridG4

--- solax/inverters/__init__.py
+++ solax/inverters/__init__.py
@@ -1,29 +1,29 @@
-from .qvolt_hyb_g3_3p import QVOLTHYBG33P
-from .x1 import X1
-from .x1_boost import X1Boost
-from .x1_hybrid_gen4 import X1HybridGen4
-from .x1_mini import X1Mini
-from .x1_mini_v34 import X1MiniV34
-from .x1_smart import X1Smart
-from .x3 import X3
+#from .qvolt_hyb_g3_3p import QVOLTHYBG33P
+#from .x1 import X1
+#from .x1_boost import X1Boost
+#from .x1_hybrid_gen4 import X1HybridGen4
+#from .x1_mini import X1Mini
+#from .x1_mini_v34 import X1MiniV34
+#from .x1_smart import X1Smart
+#from .x3 import X3
 from .x3_hybrid_g4 import X3HybridG4
-from .x3_mic_pro_g2 import X3MicProG2
-from .x3_ultra import X3Ultra
-from .x3_v34 import X3V34
-from .x_hybrid import XHybrid
+#from .x3_mic_pro_g2 import X3MicProG2
+#from .x3_ultra import X3Ultra
+#from .x3_v34 import X3V34
+#from .x_hybrid import XHybrid
 
 __all__ = [
-    "QVOLTHYBG33P",
-    "XHybrid",
-    "X1",
-    "X1Mini",
-    "X1MiniV34",
-    "X1Smart",
-    "X3V34",
+#    "QVOLTHYBG33P",
+#    "XHybrid",
+#    "X1",
+#    "X1Mini",
+#    "X1MiniV34",
+#    "X1Smart",
+#    "X3V34",
     "X3HybridG4",
-    "X3",
-    "X1Boost",
-    "X1HybridGen4",
-    "X3MicProG2",
-    "X3Ultra",
+#    "X3",
+#    "X1Boost",
+#    "X1HybridGen4",
+#    "X3MicProG2",
+#    "X3Ultra",
 ]
